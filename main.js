b3b60839dfa676de5458a04c3afd784218f7e132
Natalija Ojdanic <natalija.ojdanic@gmail.com>
2019-07-18 12:35:06 +0200

    Cetvrta lekcija

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
 var app = new Vue({
-el:'#app',
-data:{
-    product:'Socks',
-    image:'./assets/vmSocks-green-onWhite.jpg',
-    /*inventory:8*/
-    inStock:true,
-    onSale:true
-}
+    el: '#app',
+    data: {
+        product: 'Socks',
+        image: './assets/vmSocks-green-onWhite.jpg',
+        /*inventory:8*/
+        inStock: true,
+        onSale: true,
+        details: ["80 cotton", "20 polyester", "Universal"],
+        variants: [
+            {
+                variantID: 22234,
+                variantColor: "green"
+            },
+            {
+                variantID: 22235,
+                variantColor: "blue"
+            }
+        ],
+        sizes:["extra small", "small", "medium", "Large"]
+    }
 })
