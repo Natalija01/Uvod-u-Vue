1ac2550ebfdcb13728be93c7e2cd95377fd01037
Natalija Ojdanic <natalija.ojdanic@gmail.com>
2019-07-18 14:08:55 +0200

    Sedma lekcija - computed property

--- main.js
+++ main.js
@@ -2,22 +2,26 @@ var app = new Vue({
     el: '#app',
     data: {
         product: 'Socks',
-        image: './assets/vmSocks-green-onWhite.jpg',
-        /*inventory:8*/
-        inStock: false,
+        brand:'Vue Mastery',
+        selectedVariant:0,
         onSale: true,
         details: ["80 cotton", "20 polyester", "Universal"],
         variants: [
             {
                 variantID: 22234,
                 variantColor: "green",
-                variantImage: "./assets/vmSocks-green-onWhite.jpg"
+                variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                variantQuantity:10,
+                
+                
             },
             {
                 variantID: 22235,
                 variantColor: "blue",
-                variantImage: "./assets/vmSocks-blue-onWhite.jpg"
-
+                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                variantQuantity:0,
+                
+                
             }
         ],
         cart: 0
@@ -28,8 +32,27 @@ var app = new Vue({
             this.cart += 1
         },
         
-        updateProduct: function (variantImage) {
-            this.image = variantImage
+        updateProduct: function (index) {
+            this.selectedVariant = index
+            console.log(index)
+        }
+    },
+    computed: {
+        title(){
+            return this.brand + ' ' + this.product
+        },
+        image(){
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+        sale(){
+            if(this.onSale){
+                return this.brand + ' ' + this.product + ' is on sale'
+            }else{
+                return  this.brand + ' ' + this.product + ' is not on sale'
+            }
         }
     }
 })
