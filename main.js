1881f2b4cfdfe6294b98af748874dde716ec80a1
Natalija Ojdanic <natalija.ojdanic@gmail.com>
2019-07-18 12:09:23 +0200

    Druga lekcija

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var app = new Vue({
 el:'#app',
 data:{
     product:'Socks',
-    description:'Socks are cute and fluffy'
+    image:'./assets/vmSocks-green-onWhite.jpg',
+    link:'https://www.youtube.com/watch?v=uAsV5-Hv-7U&list=RDuAsV5-Hv-7U&start_radio=1'
 }
 })
