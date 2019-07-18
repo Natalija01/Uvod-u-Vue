1287d3ecbed0393a6bdfab7d78d70591c791dcbc
Natalija Ojdanic <natalija.ojdanic@gmail.com>
2019-07-18 12:22:03 +0200

    Treca lekcija

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ el:'#app',
 data:{
     product:'Socks',
     image:'./assets/vmSocks-green-onWhite.jpg',
-    link:'https://www.youtube.com/watch?v=uAsV5-Hv-7U&list=RDuAsV5-Hv-7U&start_radio=1'
+    /*inventory:8*/
+    inStock:true,
+    onSale:true
 }
 })
