959a663892b131ac16ae22f97591e272fc51fe92
Natalija Ojdanic <natalija.ojdanic@gmail.com>
2019-07-19 10:35:41 +0200

    Deseta lekcija - Forms

--- main.js
+++ main.js
@@ -39,15 +39,29 @@ Vue.component('product', {
                 :disabled="!inStock" 
                 :class="{disabledButton: !inStock}">Add to
                     cart</button>
-                <button style="display:block"v-on:click="deleteProduct()" 
-                :disabled="!inStock" 
-                :class="{disabledButton: !inStock}">Delete product</button>
+                
 
                 
             </div>
+            <div>
+                <h2>Reviews</h2>
+                <p v-if="!reviews.length">There is no reviews yet</p>
+
+                <ul>
+                    <li v-for="review in reviews">
+                    <p>Name: {{review.name}}</p>
+                    <p>Rating: {{review.rating}}</p>
+                    <p>Review: {{review.review}}</p>
+                    </li>
+                </ul>
+
+            </div>
+
+            <product-review @review-submited="addReview"></product-review>
         </div>
 
 `,
+
     data() {
         return {
             product: 'Socks',
@@ -73,6 +87,7 @@ Vue.component('product', {
 
                 }
             ],
+            reviews: []
 
         }
     },
@@ -82,12 +97,15 @@ Vue.component('product', {
             /*this. se odnosi na podatak iz data*/
             this.$emit('add-to-cart', this.variants[this.selectedVariant].variantID)
         },
-        deleteProduct: function(){
+        deleteProduct: function () {
             this.$emit('delete-product', this.variants[this.selectedVariant].variantID)
         },
         updateProduct: function (index) {
             this.selectedVariant = index
             console.log(index)
+        },
+        addReview: function (productReview) {
+            this.reviews.push(productReview)
         }
     },
     computed: {
@@ -119,6 +137,97 @@ Vue.component('product', {
         }
     }
 })
+
+Vue.component('product-review', {
+    template: `
+    <div>
+    <form class="review-form" @submit.prevent="onSubmit">
+
+    <p v-if="errors.length">
+    <b>Please correct folowing errors</b>
+    <ul>
+        <li v-for="error in errors">{{error}}</li>
+    </ul>
+    </p>
+
+    <p>
+    <label for="name">Name:</label>
+    <input id="name" v-model="name">
+    </p>
+
+    <p>
+    <label for="review">Review:</label>
+    <textarea id="review" v-model="review"></textarea>
+    </p>
+
+    <p>
+    <label for="rating">Rating:</label>
+    <select id="rating" v-model.number="rating">
+        <option>1</option>
+        <option>2</option>
+        <option>3</option>
+        <option>4</option>
+        <option>5</option>
+    </select>
+    </p>
+
+    <p>
+  
+    <label>Would you recommended this product?</label>
+    
+        <input type="radio" name="gender" v-model="question"> Yes
+        <input type="radio" name="gender"  v-model="question"> No
+   
+    </p>
+
+    <p>
+        <input type="submit" value="Submit">
+    </p>
+    </form>
+    </div>
+    `,
+
+    data() {
+        return {
+            name: null,
+            review: null,
+            rating: null,
+            question:null,
+            errors: []
+        }
+    },
+    methods: {
+        onSubmit() {
+            if (this.name && this.review && this.rating && this.question) {
+                let productReview = {
+                    name: this.name,
+                    review: this.review,
+                    rating: this.rating,
+                    question: this.question
+                }
+                this.$emit('review-submited', productReview)
+                    this.name = null,
+                    this.review = null,
+                    this.rating = null,
+                    this.question = null
+            } else {
+                if (!this.name) this.errors.push("Name required!")
+                if (!this.review) this.errors.push("Review required!")
+                if (!this.rating) this.errors.push("Rating required!")
+                if (!this.question) this.errors.push("Question required!")
+            }
+
+        }
+    }
+})
+
+
+
+
+
+
+
+
 
 var app = new Vue({
     el: '#app',
